9a0be40878be87cf1b75bfe15e089b294903af76
Leonardo / LuKi <leo.monteiro06@live.com>
2021-10-10 16:38:59 -0300

    Um novo arquivo será gerado

--- Program.cs
+++ Program.cs
@@ -13,11 +13,19 @@ namespace Portuguei
             // Pegar o caminho do arquivo
             string path = Path.GetFullPath(file[0]);
  
-            if(path == null)
+            // Verificar se o caminho do arquivo é valido
+            if(path != null)
             {
                 // Ler o código
                 string text = File.ReadAllText(path);
-                // Inserir código C++ necessário no arquivo
+
+                // Guadar nome do arquivo
+                string filename = Path.GetFileNameWithoutExtension(path);
+
+                // Criar arquivo que receberá o código C++
+                path = Path.GetDirectoryName(path) + @"\\" + filename + ".cpp";
+
+                // Inserir código C++ necessário
                 text = text.Insert(0, "#include <iostream>\n#include <stdlib.h>\n\nusing namespace std;\n\n");
 
                 // Substituir o código pelo equivalente em C++
@@ -25,18 +33,18 @@ namespace Portuguei
                 text = text.Replace("inteiro", "int");
                 text = text.Replace("real", "float");
                 text = text.Replace("vazio", "void");
-                text = text.Replace("escreva", "cout");
-                text = text.Replace("leia", "cin");
+                text = text.Replace("escreva", "cout <<");
+                text = text.Replace("leia", "cin >>");
                 text = text.Replace("terminar", "}");
                 text = text.Replace("retornar", "return");
                 text = text.Replace("sistema", "system");
 
-                // Escrever no arquivo
+                // Escrever no novo arquivo
                 File.WriteAllText(path, text);
-
+                
                 // Abrir o CMD e compilar usando GCC
-                string compile = "/C g++ -x c++ -Ofast " + path + " -o resultado.exe";
-                System.Diagnostics.Process.Start("CMD.exe", compile);
+                string compile = "/C g++ -Ofast -pipe " + path + " -o " + filename + ".exe";
+                System.Diagnostics.Process.Start("cmd.exe", compile);
             }
             else
             {
